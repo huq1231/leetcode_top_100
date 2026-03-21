ff7d63f6702e6d4c27325e3e901d58af584c39cc
huawuque <1048026810@qq.com>
2026-03-21 16:52:01 +0800

    Initial Commit

---
+++ leetcode_top_100/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  leetcode_top_100
+//
+//  Created by adia on 2026/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ leetcode_top_100/leetcode_top_100App.swift
@@ -0,0 +1,17 @@
+//
+//  leetcode_top_100App.swift
+//  leetcode_top_100
+//
+//  Created by adia on 2026/3/21.
+//
+
+import SwiftUI
+
+@main
+struct leetcode_top_100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
